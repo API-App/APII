126bebc62b674340e3d6caec13c19ee8c2c58ff5
Naseus <tdevries4i@gmail.com>
2021-04-13 19:21:45 -0400

    Fixed navigation and added detail to the toolbar

--- app/src/main/java/com/example/apii/MainActivity.java
+++ app/src/main/java/com/example/apii/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +21,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setActionBar(toolbar);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer);
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
         // Creates the menu icon in each fragment
-        //mAppBarConfiguration = new AppBarConfiguration.Builder(
-        //        (R.id.nav_view,R.id.).setDrawerLayout(drawer).build();
-        //);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_api_stream, R.id.nav_categories)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
