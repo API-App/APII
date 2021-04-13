dcd64a6f39207dd0e55bb4ef36c5e27f23846edd
datguy000 <costaandre632@gmail.com>
2021-04-13 02:04:50 -0400

    Starting things up
    Can get request to API and logs the length of the results (733).
    Also made API file where the info for each entry will be parsed

---
+++ app/src/main/java/com/example/apii/API.java
@@ -0,0 +1,11 @@
+package com.example.apii;
+
+public class API {
+    String  title;
+    String  description;
+    String  auth;
+    Boolean https;
+    String  cors;
+    String  link;
+    String  category;
+}
