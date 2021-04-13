ebee538200c4dbb1a9a951c3194ea98a0fca2632
Naseus <tdevries4i@gmail.com>
2021-04-13 14:03:06 -0400

    basic tray and colors

--- app/src/main/java/com/example/apii/MainActivity.java
+++ app/src/main/java/com/example/apii/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import android.widget.Toolbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setActionBar(toolbar);
         setContentView(R.layout.activity_main);
+        DrawerLayout drawer = findViewById(R.id.drawer);
+
+        // Creates the menu icon in each fragment
+        //mAppBarConfiguration = new AppBarConfiguration.Builder(
+        //        (R.id.nav_view,R.id.).setDrawerLayout(drawer).build();
+        //);
     }
 }
