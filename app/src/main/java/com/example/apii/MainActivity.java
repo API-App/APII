4e5bacd5723c79dd109e5ffb06d6333c2594be9b
datguy000 <costaandre632@gmail.com>
2021-04-20 00:00:33 -0400

    Recycler View Prep
    Made network request for both list of APIs and list of categories. Now ready to implement Recycler View.

--- app/src/main/java/com/example/apii/MainActivity.java
+++ app/src/main/java/com/example/apii/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.apii;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,8 +14,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.annotation.Target;
-
 import okhttp3.Headers;
 
 import androidx.appcompat.widget.Toolbar;
@@ -24,13 +24,23 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
+import com.example.apii.Models.API;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     public static final String APII_ENTRIES_URL = "https://api.publicapis.org/entries";
+    public static final String APII_CATEGORIES_URL = "https://api.publicapis.org/categories";
     public static final String TAG = "Main Activity";
+    List<API> apis;
+    List<String> categories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +60,11 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-        // creating a client to make network requests
+        // Creating a client to make network requests
         AsyncHttpClient client = new AsyncHttpClient();
 
-        // I could put this in a separate method to make the request
-        // makeRequest(client, ULR)
+        // Make request for all API entries
+        // Store in List<API> apis
         client.get(APII_ENTRIES_URL, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int i, Headers headers, JSON json) {
@@ -62,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject jsonObject = json.jsonObject;
                 try {
                     JSONArray results = jsonObject.getJSONArray("entries");
-                    Log.i(TAG, "Number of results: " + results.length());
+                    apis = API.fromJsonArray(results);
+                    Log.i(TAG, "API Len: " + String.valueOf(apis.size()));
                 } catch (JSONException e) {
                     Log.e(TAG, "Hit a json exception ", e);
                 }
@@ -74,11 +85,43 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onFailure");
             }
         });
+
+        // Make request for categories
+        // Store in List<String> categories
+        client.get(APII_CATEGORIES_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONArray results = json.jsonArray;
+                categories = fromJsonArray(results);
+                Log.i(TAG, "Category Len: " + String.valueOf(categories.size()));
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+
     }
-        @Override
-        public boolean onSupportNavigateUp() {
-            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-            return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                    || super.onSupportNavigateUp();
+
+    private List<String> fromJsonArray(JSONArray results) {
+        List<String> categories = new ArrayList<>() ;
+        for(int i=0;i<results.length();i++){
+            try{
+                categories.add(results.getString(i));
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return categories;
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
 }
