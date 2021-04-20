360a67b22d76db36787c00eb4d9ca18910e014fb
datguy000 <costaandre632@gmail.com>
2021-04-20 01:54:09 -0400

    API Recycler View Completed
    Completed Recycler View
    Stretch feature - Making the top say what the current category is that is being viewed

--- app/src/main/java/com/example/apii/MainActivity.java
+++ app/src/main/java/com/example/apii/MainActivity.java
@@ -36,10 +36,8 @@ import java.util.ListIterator;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    public static final String APII_ENTRIES_URL = "https://api.publicapis.org/entries";
     public static final String APII_CATEGORIES_URL = "https://api.publicapis.org/categories";
     public static final String TAG = "Main Activity";
-    List<API> apis;
     List<String> categories;
 
     @Override
@@ -62,29 +60,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Creating a client to make network requests
         AsyncHttpClient client = new AsyncHttpClient();
-
-        // Make request for all API entries
-        // Store in List<API> apis
-        client.get(APII_ENTRIES_URL, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int i, Headers headers, JSON json) {
-                Log.d(TAG, "onSuccess");
-                JSONObject jsonObject = json.jsonObject;
-                try {
-                    JSONArray results = jsonObject.getJSONArray("entries");
-                    apis = API.fromJsonArray(results);
-                    Log.i(TAG, "API Len: " + String.valueOf(apis.size()));
-                } catch (JSONException e) {
-                    Log.e(TAG, "Hit a json exception ", e);
-                }
-
-            }
-
-            @Override
-            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
-                Log.d(TAG, "onFailure");
-            }
-        });
 
         // Make request for categories
         // Store in List<String> categories
